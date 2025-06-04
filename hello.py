d4f7ad3ce08d72164fe1c54278a01be9b3fecc2b
David Alonso <davidalonso79@gmail.com>
2025-06-04 21:11:41 +0200

    Tipos de datos

---
+++ hello.py
@@ -0,0 +1,15 @@
+# Hola Mundo
+"""
+esto es un comentario
+y no se ejecuta
+-- """
+print("Hola Mundo")
+print('Hola Mundo 2')
+
+# Consultar el tipo de dato
+print(type("Hola Mundo 3"))
+print(type(42))
+print(type(3.14))
+print(type(1 + 2j))
+print(type([1, 2, 3]))
+print(type(True))
